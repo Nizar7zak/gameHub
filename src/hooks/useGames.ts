bddb17d63acaee7cb59151c7b7d4a0bc6f84a44c
nizar7zakout <nizar7zakout@gmail.com>
2024-08-04 00:30:41 +0300

    building came card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { AxiosError, CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {

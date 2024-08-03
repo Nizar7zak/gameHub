17a0b097ee754c34d0a073339a00cb12b5c6853b
nizar7zakout <nizar7zakout@gmail.com>
2024-08-04 00:00:24 +0300

    create custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,9 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-import { AxiosError } from 'axios';
 import { Text } from '@chakra-ui/react';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from '../hooks/useGames';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        const games = await apiClient.get<FetchGamesResponse>('/games');
-        setGames(games.data.results);
-      } catch (error) {
-        setError((error as AxiosError).message);
-      }
-    };
-    fetchGames();
-  });
+  const { games, error } = useGames();
+
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { AxiosError, CanceledError } from 'axios';
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchGames = async () => {
+      try {
+        const games = await apiClient.get<FetchGamesResponse>('/games', {
+          signal: controller.signal,
+        });
+        setGames(games.data.results);
+      } catch (error) {
+        if (error instanceof CanceledError) return;
+        setError((error as AxiosError).message);
+      }
+    };
+    fetchGames();
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;

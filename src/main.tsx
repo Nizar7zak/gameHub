7ce10e247829241a02cab5dfac95892263d5aa44
nizar7zakout <nizar7zakout@gmail.com>
2024-08-03 22:27:26 +0300

    remove default style files

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
-const rootElement = document.getElementById('root')
-ReactDOM.createRoot(rootElement).render(
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

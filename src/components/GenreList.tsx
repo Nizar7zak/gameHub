bf21646c80c6b4d77cba10c949329737bc809228
nizar7zakout <nizar7zakout@gmail.com>
2024-08-05 06:46:02 +0300

    show a skeleton while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,23 +1,50 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageURL from '../services/image-url';
 
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  if (error) return null;
+
   return (
     <List>
-      {genres.map(genre => (
-        <ListItem key={genre.id} paddingY={1.5}>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageURL(genre.image_background)}
-            />
-            <Text fontSize={'lg'}>{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
+      {isLoading
+        ? skeletons.map(skeleton => (
+            <ListItem key={skeleton} width="200px" mb={4}>
+              <HStack width="200px" alignItems="-moz-initial">
+                <Skeleton boxSize="32px" borderRadius={8} />
+                <SkeletonText
+                  width="100px"
+                  noOfLines={1}
+                  skeletonHeight="8"
+                  borderRadius={8}
+                  overflow="hidden"
+                />
+              </HStack>
+            </ListItem>
+          ))
+        : genres.map(genre => (
+            <ListItem key={genre.id} paddingY={1.5}>
+              <HStack>
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageURL(genre.image_background)}
+                />
+                <Text fontSize={'lg'}>{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          ))}
     </List>
   );
 };

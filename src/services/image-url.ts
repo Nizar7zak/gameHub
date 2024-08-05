7f991772aff8e4370205b3534080db0a2cf6566e
nizar7zakout <nizar7zakout@gmail.com>
2024-08-05 22:10:27 +0300

    handling no image from backend

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from '../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp';
 const getCroppedImageURL = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = 'media/';
   const index = url.indexOf(target) + 'media/'.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);

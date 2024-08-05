8365b6384a98f501aefc39a114098f8418c963ce
nizar7zakout <nizar7zakout@gmail.com>
2024-08-05 23:18:56 +0300

    cleaning up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,44 +23,52 @@ const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
   if (error) return null;
 
   return (
-    <List>
-      {isLoading
-        ? skeletons.map(skeleton => (
-            <ListItem key={skeleton} width="200px" mb={4}>
-              <HStack width="200px" alignItems="-moz-initial">
-                <Skeleton boxSize="32px" borderRadius={8} />
-                <SkeletonText
-                  width="100px"
-                  noOfLines={1}
-                  skeletonHeight="8"
-                  borderRadius={8}
-                  overflow="hidden"
-                />
-              </HStack>
-            </ListItem>
-          ))
-        : genres.map(genre => (
-            <ListItem key={genre.id} paddingY={1.5}>
-              <HStack>
-                <Image
-                  boxSize="32px"
-                  borderRadius={8}
-                  src={getCroppedImageURL(genre.image_background)}
-                />
-                <Button
-                  fontWeight={
-                    genre.id === selectedGenre?.id ? 'bold' : 'normal'
-                  }
-                  onClick={() => onSelectedGenre(genre)}
-                  variant="link"
-                  fontSize={'lg'}
-                >
-                  {genre.name}
-                </Button>
-              </HStack>
-            </ListItem>
-          ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" mb={3}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading
+          ? skeletons.map(skeleton => (
+              <ListItem key={skeleton} width="200px" mb={4}>
+                <HStack width="200px" alignItems="-moz-initial">
+                  <Skeleton boxSize="32px" borderRadius={8} />
+                  <SkeletonText
+                    width="100px"
+                    noOfLines={1}
+                    skeletonHeight="8"
+                    borderRadius={8}
+                    overflow="hidden"
+                  />
+                </HStack>
+              </ListItem>
+            ))
+          : genres.map(genre => (
+              <ListItem key={genre.id} paddingY={1.5}>
+                <HStack>
+                  <Image
+                    objectFit="cover"
+                    boxSize="32px"
+                    borderRadius={8}
+                    src={getCroppedImageURL(genre.image_background)}
+                  />
+                  <Button
+                    whiteSpace="normal"
+                    textAlign="left"
+                    fontWeight={
+                      genre.id === selectedGenre?.id ? 'bold' : 'normal'
+                    }
+                    onClick={() => onSelectedGenre(genre)}
+                    variant="link"
+                    fontSize={'lg'}
+                  >
+                    {genre.name}
+                  </Button>
+                </HStack>
+              </ListItem>
+            ))}
+      </List>
+    </>
   );
 };
 

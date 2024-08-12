4eef0b1b42078a9e7672c1fb42b5868980a215f4
nizar7zakout <nizar7zakout@gmail.com>
2024-08-12 19:17:05 +0300

    install and setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
 import theme from './theme';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 const rootElement = document.getElementById('root');
 ReactDOM.createRoot(rootElement as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
